23ffda874da2a912ff804ceae2cbd5581828972c
popcorngirl94 <grishmasht17@gmail.com>
2023-06-06 12:53:15 +0545

    adding files

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Homecontroller;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    dd("Welcome");
+    return view('welcome');
+});
+Route::get('/login', [LoginController::class, 'authenticate']);
+
+Route::get('/home', [HomeController::class,'index']);
+
+Route::get('/products', [ProductController::class,'index']);
+  
+Route::get('/product/{slug}', [ProductController::class,'show']);
+
+Route::post('/cart', [CartController::class,'add']);
+
+Route::get('/cart', [CartController::class,'show']);
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/categories',[CategoryController::class,'getAction']);
